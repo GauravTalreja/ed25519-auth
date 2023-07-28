7c70b5f083aa9376fb62920a3fb635a9686a0c3b
GT <59036640+GauravTalreja@users.noreply.github.com>
2023-07-28 14:14:07 -0400

    timestamp

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use axum::{extract::Path, http::StatusCode, routing::post, Extension, Json, Router};
-use serde::Deserialize;
-use ssh_key::{PublicKey, Signature, SshSig};
+use axum::{extract::Path, http::StatusCode, routing::post, Extension, Router};
+use chrono::Utc;
+use ssh_key::{PublicKey, SshSig};
 use std::{
-    collections::{BTreeSet, HashMap},
+    collections::HashMap,
     net::SocketAddr,
     sync::{Arc, RwLock},
 };
+
+const NAMESPACE: &str = "s3";
 
 #[tokio::main]
 async fn main() {
@@ -17,20 +19,11 @@ async fn main() {
         ("test5".to_owned(), None),
     ]);
 
-    let signatures = HashMap::from([
-        ("test1".to_owned(), BTreeSet::<Signature>::new()),
-        ("test2".to_owned(), BTreeSet::<Signature>::new()),
-        ("test3".to_owned(), BTreeSet::<Signature>::new()),
-        ("test4".to_owned(), BTreeSet::<Signature>::new()),
-        ("test5".to_owned(), BTreeSet::<Signature>::new()),
-    ]);
-
     let app = Router::new()
         .route("/register/:uid", post(register))
         .route("/login/:uid", post(login))
         .route("/attack", post(login))
-        .layer(Extension(Arc::new(RwLock::new(users))))
-        .layer(Extension(Arc::new(RwLock::new(signatures))));
+        .layer(Extension(Arc::new(RwLock::new(users))));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
     axum::Server::bind(&addr)
@@ -63,38 +56,20 @@ async fn register(
     }
 }
 
-#[axum::debug_handler]
 async fn login(
     users: Extension<Arc<RwLock<HashMap<String, Option<PublicKey>>>>>,
-    signatures: Extension<Arc<RwLock<HashMap<String, BTreeSet<Signature>>>>>,
     Path(uid): Path<String>,
     body: String,
 ) -> StatusCode {
+    let timestamp = Utc::now().timestamp();
     match SshSig::from_pem(body) {
         Ok(sig) => match sig.algorithm() {
             ssh_key::Algorithm::Ed25519 => match users.read().unwrap().get(&uid) {
                 Some(saved_key) => match saved_key {
                     Some(saved_key) => {
-                        if sig.public_key() == saved_key.key_data() {
-                            if !signatures
-                                .read()
-                                .unwrap()
-                                .get(&uid)
-                                .unwrap()
-                                .contains(sig.signature())
-                            {
-                                signatures
-                                    .write()
-                                    .unwrap()
-                                    .get_mut(&uid)
-                                    .unwrap()
-                                    .insert(sig.signature().clone());
-                                StatusCode::OK
-                            } else {
-                                StatusCode::IM_A_TEAPOT
-                            }
-                        } else {
-                            StatusCode::IM_A_TEAPOT
+                        match saved_key.verify(NAMESPACE, timestamp.to_string().as_bytes(), &sig) {
+                            Ok(_) => StatusCode::OK,
+                            Err(_) => StatusCode::IM_A_TEAPOT,
                         }
                     }
                     None => StatusCode::IM_A_TEAPOT,
@@ -105,67 +80,4 @@ async fn login(
         },
         Err(_) => StatusCode::IM_A_TEAPOT,
     }
-}
-
-#[axum::debug_handler]
-async fn attack(body: Json<Vec<PeekRaw>>) -> StatusCode {
-    todo!()
-}
-
-#[derive(Deserialize)]
-struct PeekRaw {
-    pub args: Vec<String>,
-    pub data: String,
-    pub status: u16,
-    pub message: String,
-}
-
-struct PeekRegister {
-    pub uid: String,
-    pub public_key: PublicKey,
-}
-
-struct PeekLogin {
-    pub uid: String,
-    pub sig: SshSig,
-}
-
-impl TryInto<PeekLogin> for PeekRaw {
-    fn try_into(self) -> Result<PeekLogin, Self::Error> {
-        if self.status == 200 {
-            if let Some(arg) = self.args.get(0) {
-                if arg == "login" {
-                    if let Ok(sig) = SshSig::from_pem(self.data) {
-                        return Ok(PeekLogin {
-                            uid: self.args[1].clone(),
-                            sig,
-                        });
-                    }
-                }
-            }
-        }
-        Err(())
-    }
-
-    type Error = ();
-}
-
-impl TryInto<PeekRegister> for PeekRaw {
-    fn try_into(self) -> Result<PeekRegister, Self::Error> {
-        if self.status == 200 {
-            if let Some(arg) = self.args.get(0) {
-                if arg == "register" {
-                    if let Ok(public_key) = PublicKey::from_openssh(&self.data) {
-                        return Ok(PeekRegister {
-                            uid: self.args[1].clone(),
-                            public_key,
-                        });
-                    }
-                }
-            }
-        }
-        Err(())
-    }
-
-    type Error = ();
 }
